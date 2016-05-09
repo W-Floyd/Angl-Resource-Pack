0d731e90c604baa3559f07b05d2a77b20961d904
William Floyd <william.png2000@gmail.com>
2016-05-09 14:32:34 +1000

    Added birch and big oak planks!

---
+++ src/assets/minecraft/textures/blocks/planks_birch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+BASE="planks_oak"
+OVERLAY="planks_birch"
+
+inkscape \
+--export-dpi=$(echo "(90*"$1")/128" | bc) \
+--export-png \
+$OVERLAY".png" $OVERLAY".svg"
+
+REMOVE=0
+if ! [ -a $BASE".png" ]; then
+	REMOVE=1
+	inkscape \
+	--export-dpi=$(echo "(90*"$1")/128" | bc) \
+	--export-png \
+	$BASE".png" $BASE".svg"
+fi
+
+composite -compose Screen $OVERLAY".png" $BASE".png" $OVERLAY"_.png"
+
+rm $OVERLAY".png"
+
+if [ $REMOVE = 1 ]; then
+	rm $BASE".png"
+fi
+
+mv $OVERLAY"_.png" $OVERLAY".png"
+
+exit
