56186bb4478d2bac5cbc10dd989219b9693c27a9
William Floyd <william.png2000@gmail.com>
2016-05-09 15:33:35 +1000

    Added a script to give a listing of files that end up in the rendered pack.

---
+++ What-Is-Done.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+RES="16"
+
+bash Render.sh $RES &> /dev/null
+
+cd "Angl-"$RES"px"
+
+TRIMDIR=$PWD
+
+recurse () {
+
+CURDIR=$PWD
+
+RELATIVE=$(echo "."$(echo $CURDIR | sed 's/'$(echo $TRIMDIR | sed 's/\//\\\//g')'//')"/")
+
+for file in $(ls -1 *.* 2> /dev/null); do
+	echo $RELATIVE""$file
+done
+
+for dir in $(ls -d ./*/ 2> /dev/null); do
+	cd $dir
+	recurse
+	cd ../
+done
+
+}
+
+recurse
+
+cd ../
+
+rm -r "Angl-"$RES"px/"
+
+exit
