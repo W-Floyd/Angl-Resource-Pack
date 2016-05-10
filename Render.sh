d8ad899f771cc6ba7db0e6f8d87e43472aa5669d
William Floyd <william.png2000@gmail.com>
2016-05-10 13:02:05 +1000

    Added feature not to render files that feature "\!ignore\!" in their name

--- Render.sh
+++ Render.sh
@@ -40,6 +40,8 @@ for image in $(ls *.svg 2> /dev/null); do
 
 	if [ $image = $RENDERFILE ]; then
 
+	if [ -z $(echo $image | grep -o "!ignore!"); then
+
 	if [ -a $(echo $(basename $image .svg)".sh") ]; then
 		bash $(echo $(basename $image .svg)".sh") $RES
 		rm $(echo $(basename $image .svg)".sh")
@@ -50,6 +52,8 @@ for image in $(ls *.svg 2> /dev/null); do
 			--export-png \
 			$(basename $image .svg)".png" $image
 		fi
+	fi
+
 	fi
 
 	fi
