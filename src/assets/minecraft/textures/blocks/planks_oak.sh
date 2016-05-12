94d4f3c0f4dd77f726df034f1e76107f874421de
William Floyd <william.png2000@gmail.com>
2016-05-12 12:24:21 +1000

    Moved generic plank overlay away from oak name

--- src/assets/minecraft/textures/blocks/planks_oak.sh
+++ src/assets/minecraft/textures/blocks/planks_oak.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 BASE="~ignore~planks_oak"
-OVERLAY="planks_oak"
+OVERLAY="~ignore~planks"
 
-inkscape \
---export-dpi=$(echo "(90*"$1")/128" | bc -l | rev | sed 's/0*//' | rev) \
---export-png \
-$OVERLAY".png" $OVERLAY".svg"
+DPI=$(echo "(90*"$1")/128" | bc -l | rev | sed 's/0*//' | rev)
+
+# Takes DPI and FILE in
+render () {
+inkscape --export-dpi=$1 --export-png $2".png" $2".svg"
+}
+
+render $DPI $OVERLAY
 
 REMOVE=0
 if ! [ -a $BASE".png" ]; then
 	REMOVE=1
-	inkscape \
-	--export-dpi=$(echo "(90*"$1")/128" | bc -l | rev | sed 's/0*//' | rev) \
-	--export-png \
-	$BASE".png" $BASE".svg"
+	render $DPI $BASE
 fi
 
 composite -compose Multiply $OVERLAY".png" $BASE".png" $OVERLAY"_.png"
