ae02c91e18c448dacb4d1ea50b62235a77ce0286
William Floyd <william.png2000@gmail.com>
2016-05-13 09:42:26 +1000

    Toned down plank and log shading.

--- Checks/Check-Bookshelf.sh
+++ Checks/Check-Bookshelf.sh
@@ -6,32 +6,42 @@ else
 	RES=128
 fi
 
-TEXTURE="bookshelf"
+TEXTURE1="planks_oak"
+TEXTURE2="bookshelf"
 
 cd ../
 
-bash Render.sh $RES $TEXTURE
+bash Render.sh $RES $TEXTURE1
 
-mv "./Angl-"$RES"px/assets/minecraft/textures/blocks/"$TEXTURE".png" ./Checks/$TEXTURE".png"
+mv "./Angl-"$RES"px/assets/minecraft/textures/blocks/"$TEXTURE1".png" ./Checks/$TEXTURE1".png"
+
+rm -r "./Angl-"$RES"px/"
+
+bash Render.sh $RES $TEXTURE2
+
+mv "./Angl-"$RES"px/assets/minecraft/textures/blocks/"$TEXTURE2".png" ./Checks/$TEXTURE2".png"
 
 rm -r "./Angl-"$RES"px/"
 
 cd Checks
 
-if [ -a "montage_"$TEXTURE".png" ]; then
-	rm "montage_"$TEXTURE".png"
+if [ -a "montage_"$TEXTURE2".png" ]; then
+	rm "montage_"$TEXTURE2".png"
 fi
 
-if [ -a "montage_"$TEXTURE"_.png" ]; then
-	rm "montage_"$TEXTURE"_.png"
+if [ -a "montage_"$TEXTURE2"_.png" ]; then
+	rm "montage_"$TEXTURE2"_.png"
 fi
 
-IMGSEQ=$(for tile in $(seq 1 9); do echo -n $TEXTURE".png "; done)
+IMGSEQ1=$(for tile in $(seq 1 3); do echo -n $TEXTURE1".png "; done)
+IMGSEQ2=$(for tile in $(seq 1 3); do echo -n $TEXTURE2".png "; done)
 
-montage -geometry +0+0 -tile 3x3 $IMGSEQ "montage_"$TEXTURE".png"
+montage -geometry +0+0 -tile 3x3 $IMGSEQ1 $IMGSEQ2 $IMGSEQ1 "montage_"$TEXTURE".png"
 
-montage -geometry +1+1 -tile 3x3 $IMGSEQ "montage_"$TEXTURE"_.png"
+montage -geometry +1+1 -tile 3x3 $IMGSEQ1 $IMGSEQ2 $IMGSEQ1 "montage_"$TEXTURE"_.png"
 
-rm $TEXTURE".png"
+rm $TEXTURE1".png"
+
+rm $TEXTURE2".png"
 
 exit
