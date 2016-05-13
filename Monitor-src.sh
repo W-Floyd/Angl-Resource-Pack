28816982e6df0b6ceddecd45a79e57574049d060
William Floyd <william.png2000@gmail.com>
2016-05-13 21:11:14 +1000

    Added cobblestone, check script and renamed some scripts.

--- Monitor-src.sh
+++ Monitor-src.sh
@@ -2,7 +2,7 @@
 
 while true; do
 	inotifywait -qq -r -e modify,attrib,close_write,move,create,delete ./src/
-	bash tar-src.sh &> /dev/null
+	bash Tar-src.sh &> /dev/null
 	echo "src.tar updated"
 done
 
