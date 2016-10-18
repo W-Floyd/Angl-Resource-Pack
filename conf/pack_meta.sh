67456eee1651360a2b17c86a79475dcf8ac6eb04
W-Floyd <william.png2000@gmail.com>
2016-10-18 14:34:41 +1000

    Still working things out, getting fucntions set up mostly

---
+++ conf/pack_meta.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+$(date +%Y-%m-%d_%H-%M-%S)
+
+exit
--- render.sh
+++ render.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+###############################################################
 # Will render the resource pack at the given resolution
 # Or, if no inputs are given, 128px
-# If you want to render only one image, specify the resolution,
-# then the file, relative to the top level directory.
-# Example
-# ./render.sh 512 ./assets/minecraft/textures/blocks/bookshelf.svg
-#
-# Will also work on non-image files
-# Example
-# ./render.sh 128 ./pack.mcmeta
+###############################################################
+# Set up variables
+###############################################################
 
-__name="Angl"
+__pack_name="Angl"
+
+__tmp_directory='/tmp/texpack/'
+
+__catalogue='catalogue.xml'
 
 if ! [ -z $1 ]; then
 	__resolution=$1
@@ -18,19 +18,40 @@ else
 	__resolution=128
 fi
 
-__date=$(date +%Y-%m-%d_%H-%M-%S)
-
-__directory=$(echo $__name"-"$__resolution"px/")
+__directory=$(echo $__pack_name"-"$__resolution"px/")
 
 cp -r src/ $__directory
 
-if [ -a /tmp/texpack/rendered ]; then
-	rm /tmp/texpack/rendered
+if ! [ -d $__tmp_directory ]; then
+	mkdir /tmp/texpack/
 fi
 
+###############################################################
+# Set up functions
+###############################################################
+
+source ./conf/__functions.sh
+
+###############################################################
+# Set up working space
+###############################################################
 
 
-for line in $(cat catalogue.xml | grep -i '<NAME>'); do
+
+###############################################################
+# Split all files into their own .xml records
+###############################################################
+
+for __range in $(__get_range $__catalogue ITEM); do
+	__name=$(__get_value "$(__read_range $__catalogue $__range)" NAME)
+	__tmp_name=$(echo $__tmp_directory""$__name".xml")
+	mkdir -p $(dirname $__tmp_name)
+	__read_range $__catalogue $__range > $__tmp_name
+done
+
+###############################################################
+# 
+###############################################################
 
 
 
