ebaaa0a2b2162182b049967c9f918b347c57b04f
W-Floyd <william.png2000@gmail.com>
2016-10-18 18:19:46 +1000

    Some fixes, still not working, need to debug a greop function I think.

--- render.sh
+++ render.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#set -x
+#trap read debug
+
+#
+# NEED TO CHECK __get_value
+#
 ###############################################################
 # Will render the resource pack at the given resolution
 # Or, if no inputs are given, 128px
@@ -12,13 +18,13 @@ __tmp_directory='/tmp/texpack/'
 
 __catalogue='catalogue.xml'
 
-if ! [ -z $1 ]; then
-	__resolution=$1
+if ! [ -z "$1" ]; then
+	__resolution="$1"
 else
 	__resolution=128
 fi
 
-__directory=$(echo $__pack_name"-"$__resolution"px/")
+__directory=$(echo "${__pack_name}-${__resolution}px/")
 
 if ! [ -d $__tmp_directory ]; then
 	mkdir $__tmp_directory
@@ -38,11 +44,11 @@ source ./conf/__functions.sh
 ###############################################################
 
 for __range in $(__get_range $__catalogue ITEM); do
-	__read_range $__catalogue $__range > $__tmp_directory"readrangetmp"
-	__name=$(__get_value $__tmp_directory"readrangetmp" NAME)
-	__tmp_name=$(echo $__tmp_directory"xml/"$__name".xml")
-	mkdir -p $(dirname $__tmp_name)
-	__read_range $__catalogue $__range > $__tmp_name
+	__read_range "$__catalogue" "$__range" > $__tmp_directory"readrangetmp"
+	__name=$(__get_value "${__tmp_directory}readrangetmp" NAME)
+	__tmp_name=$(echo "${__tmp_directory}xml/${__name}\.xml")
+	mkdir -p "$(dirname "$__tmp_name")"
+	__read_range "$__catalogue" "$__range" > "$__tmp_name"
 done
 
 rm $__tmp_directory"readrangetmp"
@@ -51,45 +57,44 @@ __tmppwd=$(pwd)
 
 cd $__tmp_directory'xml/'
 
-find $(pwd) | grep '\.xml' > ../listing
+find "$(pwd)" | grep '\.xml' > ../listing
 
 touch ../rendered
 
-cd $__tmppwd
+cd "$__tmppwd"
 
 ###############################################################
 # Set up working space
 ###############################################################
 
-cp -r src/ $__directory
+cp -r src/ "$__directory"
 
-cp -r conf/ './'$__directory'/conf'
+cp -r conf/ './'"$__directory"
 
-cd $__directory
+cd "$__directory"
 
 ###############################################################
 # Start rendering
 ###############################################################
 
-until [ $(cat $__tmp_directory'listing') = $(cat $__tmp_directory'rendered') ]; do
+until [ "$(cat "$__tmp_directory"'listing' | sha1sum | sed 's/ .*//')" = "$(cat "$__tmp_directory"'rendered' | sha1sum | sed 's/ .*//')" ]; do
 
-for __config in $(cat $__tmp_directory'listing' | grep '\.xml'); do
-	__depends=$(__get_value $__config DEPENDS)
-	if ! [ -z $__depends ]; then
+for __config in $(cat "$__tmp_directory"'listing' | grep '\.xml'); do
+	if ! [ -z "$(__get_value "$__config" DEPENDS)" ]; then
 		break
 	fi
-	__name=$(__get_value $__config NAME)
-	__config_script=$(__get_value $__config CONFIG)
-	cp $__config_script $__dir
-	__tmp_resolution=$(__get_value $__config SIZE)
-	if [ -z $__tmp_resolution ]; then
-		__tmp_resolution=$__resolution
+	__name="$(__get_value "$__config" NAME)"
+	__config_script="$(__get_value "$__config" CONFIG)"
+	cp "$__config_script" ./
+	__tmp_resolution="$(__get_value "$__config" SIZE)"
+	if [ -z "$__tmp_resolution" ]; then
+		__tmp_resolution="$__resolution"
 	fi
-	eval $(echo "$(basename $__config_script)b$__tmp_resolution $(__get_value $__config OPTIONS)")
-	echo "$__name" >> $__tmp_directory'rendered'
-	rm $(basename $__config_script)
-	for __config in $(cat $__tmp_directory'listing' | grep '\.xml'); do
-		__set_value $__config DEPENDS $(__get_value $__config DEPENDS | sed 's/'"$__name"'//')
+	eval "$(echo '\./'"$(basename $__config_script)" "$__tmp_resolution" "$(__get_value $__config OPTIONS)")"
+	echo "$__name" >> "$__tmp_directory"'rendered'
+	rm "$(basename "$__config_script")"
+	for __config2 in $(cat "$__tmp_directory"'listing'); do
+		__set_value "$__config2" DEPENDS "$(__get_value "$__config2" DEPENDS | sed 's/'"$__name"'//')"
 	done
 done
 
@@ -100,19 +105,19 @@ done
 ###############################################################
 
 for __config in $(cat $__tmp_directory'listing' | grep '\.xml'); do
-	if [ $(__get_value $__config KEEP) = NO ]; then
-		rm $(__get_value $__config NAME)
+	if [ "$(__get_value $__config KEEP)" = NO ]; then
+		rm '\./'"$(__get_value $__config NAME)"
 	fi
-	__get_value $__config CLEANUP >> $__tmp_directory'cleanup'
+	__get_value "$__config" CLEANUP >> "$__tmp_directory"'cleanup'
 done
 
-for __file in $(cat $__tmp_directory'cleanup' | sort | uniq); do
+for __file in $(cat "$__tmp_directory"'cleanup' | sort | uniq); do
 	rm '\./'"$__file"
 done
 
 rm -rf ./conf/
 
-rm -rf $__tmp_directory
+rm -rf "$__tmp_directory"
 
 cd ../
 
