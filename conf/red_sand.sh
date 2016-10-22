602fc5ce956739df5e01b3e50d57b687ae1ca3ae
W-Floyd <william.png2000@gmail.com>
2016-10-22 21:10:57 +1000

    Added red sand, and modified sand to use a generic overlay with red sand.

---
+++ conf/red_sand.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ./assets/minecraft/textures/blocks/
+
+__overlay red_sand_colour.png sand_overlay.png red_sand.png
+
+cd ../../../../
+
+exit
---
+++ conf/sand.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ./assets/minecraft/textures/blocks/
+
+__overlay sand_colour.png sand_overlay.png sand.png
+
+cd ../../../../
+
+exit
