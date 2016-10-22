edf911832b9d1fa497a3e4c48dde6c789c5fa5c0
W-Floyd <william.png2000@gmail.com>
2016-10-22 16:02:05 +1000

    Added grass_top and made stained glass properly coloured

--- conf/glass.sh
+++ conf/glass.sh
@@ -4,9 +4,17 @@ cd ./assets/minecraft/textures/blocks/
 
 cp ../../../../colour_"$2".png colour_"$2".png
 
-__clip_src_in glass.png colour_"$2".png glass_"$2".png
+__fade colour_"$2".png colour_"$2"_.png 0.25
+
+__clip_src_in glass.png colour_"$2".png glass_"$2"_.png
+
+__overlay colour_"$2"_.png glass_"$2"_.png glass_"$2".png
 
 rm colour_"$2".png
+
+rm colour_"$2"_.png
+
+rm glass_"$2"_.png
 
 cd ../../../../
 
