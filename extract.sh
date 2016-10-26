1bedd1cbda7f518e589dc6b5f141ff3379ffa6cb
W-Floyd <william.png2000@gmail.com>
2016-10-26 16:44:24 +1000

    Added -p option to extract script to help speed things up in future, when batching all check scripts. Added dirt and grass check script.

--- extract.sh
+++ extract.sh
@@ -4,9 +4,27 @@ __name="Angl"
 
 __size="${1}"
 
-__files="$(echo "$@" | sed 's/ /\n/g' | sed '1d')"
+if [ "${2}" = '-p' ]; then
+	__pre_rendered='1'
+else
+	__pre_rendered='0'
+fi
 
-./make-pack.sh "${__size}"
+if [ "${__pre_rendered}" = '1' ]; then
+
+	__files="$(echo "$@" | sed 's/ /\n/g' | sed '1,2d')"
+	
+else
+
+	__files="$(echo "$@" | sed 's/ /\n/g' | sed '1d')"
+	
+fi
+
+if [ "${__pre_rendered}" = '0' ]; then
+
+	./make-pack.sh "${__size}"
+	
+fi
 
 cd "${__name}-${__size}px"
 
