50f517e34fcb15fa489e596644c7e673da612717
W-Floyd <william.png2000@gmail.com>
2016-10-30 12:29:41 +1000

    Made bed sides 1/16 higher to work in minecraft, since 9/16 is oh so logical.

--- conf/bed.sh
+++ conf/bed.sh
@@ -4,22 +4,22 @@ cd ./assets/minecraft/textures/blocks/
 
 case "$2" in
 	"bed_feet_end")
-		__shift="0.5"
+		__shift="0.5625"
 		__rotate="-1"
 		__start="0 0"
 		;;
 	"bed_feet_side")
-		__shift="0.5"
+		__shift="0.5625"
 		__rotate="0"
 		__start="1 1"
 		;;
 	"bed_head_side")
-		__shift="0.5"
+		__shift="0.5625"
 		__rotate="0"
 		__start="2 1"
 		;;
 	"bed_head_end")
-		__shift="0.5"
+		__shift="0.5625"
 		__rotate="1"
 		__start="3 0"
 		;;
