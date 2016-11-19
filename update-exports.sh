74056d52fe33a178e2b725e36ebab3c34e236456
William Floyd <william.png2000@gmail.com>
2016-11-20 00:30:06 +1000

    Ended up not needing the secure file, but I'll keep it just in case. Should now auto tag releases.

--- update-exports.sh
+++ update-exports.sh
@@ -5,18 +5,22 @@ __date=$(date +%Y-%m-%d_%H-%M-%S)
 ./make-pack.sh
 
 for __file in $(ls -1 *.zip); do
-	mv $__file ../Angl-Resource-Pack-Export/$__file
+	mv "${__file}" "../Angl-Resource-Pack-Export/${__file}"
 done
 
 cd ../Angl-Resource-Pack-Export
 
-sed -i '3s/.*/'$__date'/' README.md
+sed -i '3s/.*/'"${__date}"'/' README.md
 
 git add *
 
-git commit -m ""$__date""
+git commit -m "$__date"
 
 git push
+
+git tag -a "${__date}" -m "Exports updated at ${__date}"
+
+git push --tags
 
 cd ../Angl-Resource-Pack
 
