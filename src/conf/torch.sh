f85e3c3efc682e3b49cc4408f4b33d021c029bca
William Floyd <william.png2000@gmail.com>
2016-12-26 19:04:49 +1100

    Added torch.

---
+++ src/conf/torch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+__pushd ./assets/minecraft/textures/blocks/
+
+__overlay "torch_on_base.png" "torch_on_shaft.png" "torch_on.png"
+
+
+__popd
+
+exit
---
+++ src/conf/torch_shaft.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+__pushd ./assets/minecraft/textures/blocks/
+
+__clip_src_in "torch_on_cutout.png" "log_oak.png" "torch_on_shaft.png"
+
+__popd
+
+exit
