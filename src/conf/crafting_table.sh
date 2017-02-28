633d53bf727bab970199371019189b475b24061a
William Floyd <william.png2000@gmail.com>
2017-02-28 17:00:06 +1000

    Merged crafting table scripts and added crafting table side.

---
+++ src/conf/crafting_table.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ./assets/minecraft/textures/blocks/
+
+__overlay planks_oak.png crafting_table_${2}_overlay.png crafting_table_${2}_.png
+
+__overlay crafting_table_${2}_.png crafting_table_${2}_decal.png crafting_table_${2}.png
+
+rm crafting_table_${2}_.png
+
+cd ../../../../
+
+exit
