cc51fd815659554a7db553341eadc725f9111355
William Floyd <william.png2000@gmail.com>
2017-03-10 15:22:29 +1000

    Added OPTIONAL field, along with log and plank demo images.

--- config.sh
+++ config.sh
@@ -5,42 +5,12 @@ compgen -A variable > /tmp/tmpvars
 # Start customizing from here
 ################################################################
 
-# Name of the pack, defaults to folder name if missing
 __name='Angl'
 
-# Default render sizes
 __sizes='32 64 128 256 512 1024'
 
-# Temporary directory to use. Defaults to /tmp/texpack${__pid}
-# if missing
-#__tmp_dir="$(pwd)/tmp/${__pid}"
-
-# Script to use for mobile conversion. No default currently,
-# will disable mobile conversion if missing
 __smelt_make_mobile_bin='./convert_to_mobile.sh'
 
-# Custom functions to use. Please follow the same format as the
-# standard functions.sh file. May include both image functions
-# and any other you find you need when rendering images
-#__custom_function_bin='./functions.sh'
-
-# Whether or not to render in quick mode (if applicable).
-# Defaults to quick mode if missing
-__quick='1'
-
-# Whether or not the final images should be optimized
-__should_optimize='0'
-
-# Largest size to optimize
-__max_optimize='1024'
-
-# Whether or not to ignore the max optimize size
-__ignore_max_optimize='0'
-
-# What optimizer to use. May be a custom optimizer, so long as a
-# function named __optimize_<OPTIMIZER> exists, and replaces the
-# given file. Defaults to an existing optimizer, selected from a
-# list and by availability
 __optimizer='optipng'
 
 ################################################################
---
+++ src/conf/demos/wood.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+__woods='acacia
+big_oak
+birch
+jungle
+oak
+spruce'
+
+__num_woods="$(echo "${__woods}" | wc -l )"
+
+echo "${__woods}" | while read -r __wood; do
+
+    __tile "./assets/minecraft/textures/blocks/${2}_${__wood}.png" 2x2 "./demos/${2}_${__wood}.png"
+
+done
+
+__custom_tile $(echo "${__woods}" | while read -r __wood; do echo -n "./demos/${2}_${__wood}.png "; done) "${__num_woods}x1" "${3}" "./demos/${2}_${3}.png"
+
+echo "${__woods}" | while read -r __wood; do
+
+    rm "./demos/${2}_${__wood}.png"
+
+done
+
+exit
