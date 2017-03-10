f6040d3c96a4798b9a0e112f1f434261ff858560
William Floyd <william.png2000@gmail.com>
2017-03-10 15:41:45 +1000

    Fixed spacing, so individual textures I seen.

--- src/conf/demos/wood.sh
+++ src/conf/demos/wood.sh
@@ -11,11 +11,11 @@ __num_woods="$(echo "${__woods}" | wc -l )"
 
 echo "${__woods}" | while read -r __wood; do
 
-    __tile "./assets/minecraft/textures/blocks/${2}_${__wood}.png" 2x2 "./demos/${2}_${__wood}.png"
+    __tile "./assets/minecraft/textures/blocks/${2}_${__wood}.png" 2x2 "./demos/${2}_${__wood}.png" "${3}"
 
 done
 
-__custom_tile $(echo "${__woods}" | while read -r __wood; do echo -n "./demos/${2}_${__wood}.png "; done) "${__num_woods}x1" "${3}" "./demos/${2}_${3}.png"
+__custom_tile $(echo "${__woods}" | while read -r __wood; do echo -n "./demos/${2}_${__wood}.png "; done) "${__num_woods}x1" "0" "./demos/${2}_${3}.png"
 
 echo "${__woods}" | while read -r __wood; do
 
