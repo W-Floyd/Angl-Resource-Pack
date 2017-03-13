c6da2c5364ed9e38f7d78ba865a140ff23f601c3
William Floyd <william.png2000@gmail.com>
2017-03-13 15:23:03 +1000

    Added cobblestone and ore demos.

---
+++ src/conf/demos/ores.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+__ores='coal_ore
+redstone_ore'
+
+__num_ores="$(echo "${__ores}" | wc -l )"
+
+__custom_tile \
+$(for __num in $(seq 1 "$((__num_ores+3))"); do echo -n './assets/minecraft/textures/blocks/stone.png '; done) \
+$(echo "${__ores}" | while read -r __ore; do echo -n "./assets/minecraft/textures/blocks/${__ore}.png "; done) \
+$(for __num in $(seq 1 "$((__num_ores+3))"); do echo -n './assets/minecraft/textures/blocks/stone.png '; done) \
+"$((__num_ores+2))x$((3))" \
+"${2}" \
+"./demos/ores_${2}.png"
+
+exit
