90b5a2155fb7ad9da2d3b4851aa302849aa49f55
William Floyd <william.png2000@gmail.com>
2017-04-19 09:47:01 +1000

    Removed extra header and tail from config.sh, since it's now built into furnace.

--- config.sh
+++ config.sh
@@ -1,9 +1,4 @@
 #furnaceconfig#
-echo 'something to use a pipe' | rev > /dev/null
-compgen -A variable > /tmp/tmpvars
-################################################################
-# Start customizing from here
-################################################################
 
 __name='Angl'
 
@@ -18,11 +13,3 @@ __max_optimize='512'
 __max_optional='2048'
 
 __should_optimize='1'
-
-################################################################
-# Stop customizing from here
-################################################################
-compgen -A variable > /tmp/tmpvars2
-for __variable in $(grep -Fxvf /tmp/tmpvars /tmp/tmpvars2); do
-    export "${__variable}"
-done
