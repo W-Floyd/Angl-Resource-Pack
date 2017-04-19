fee6ad78a0b702a1c1e48eae38f654683bad99a4
William Floyd <william.png2000@gmail.com>
2017-04-19 13:24:07 +1000

    Tuned concrete powder.

--- src/conf/concrete_powder.sh
+++ src/conf/concrete_powder.sh
@@ -4,7 +4,7 @@ __pushd ./assets/minecraft/textures/blocks/
 
 cp "../../../../colour_${2}.png" "./colour_${2}.png"
 
-__multiply "./colour_${2}.png" './concrete_powder_generic_overlay.png' "./concrete_powder_${2}.png"
+__overlay "./colour_${2}.png" './concrete_powder_generic_overlay.png' "./concrete_powder_${2}.png"
 
 rm "./colour_${2}.png"
 
